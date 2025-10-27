1b070bea93b88624d3a42d1786e1fd0a107b5da3
Simone Tomè <simone.tome@polimi.it>
2025-10-27 11:01:09 +0100

    refactor: data_loading

--- test_src/fetch_data.py
+++ test_src/fetch_data.py
@@ -5,12 +5,74 @@ Script to fetch data from UCI repository
 import sys
 import os
 import pandas as pd
-from data_loading import load_uciml
+from ucimlrepo import fetch_ucirepo
 import pickle
-      
+import numpy as np
+
+
+def load_uciml(id: int = None, name: str = None, na_drop: bool = False):
+    """
+    Function to load data from the UCI Machine learning repository
+    Args:
+        id:
+        name:
+    Returns:
+    """
+
+    if id is None:
+        dataset = fetch_ucirepo(name=name)
+    else:
+        dataset = fetch_ucirepo(id=id)
+
+    # access data
+    X = dataset.data.features
+    if na_drop:
+        X = X[pd.isna(X).sum()[~np.bool(pd.isna(X).sum().values)].index]
+    y = dataset.data.targets
+
+    cat_variables = dataset.variables.loc[(dataset.variables['type'] == 'Categorical') | (dataset.variables['type'] == 'Binary'), 'name']
+    num_variables = dataset.variables.loc[(dataset.variables['type'] != 'Categorical') & (dataset.variables['type'] != 'Binary'), 'name']
+
+    features = dataset.data.features.columns
+
+    cat_variables = cat_variables[cat_variables.isin(features)]
+    num_variables = num_variables[num_variables.isin(features)]
+
+    obj_cols = X.select_dtypes(include='object').columns
+
+    X = X.copy()
+    X.loc[:,obj_cols] = X.loc[:,obj_cols].apply(lambda col: col.astype('category').cat.codes.astype(float))
+    X.loc[:, num_variables] = X.loc[:, num_variables].astype('float64')
+
+    cat_variables_in_X = cat_variables[cat_variables.isin(X.columns)]
+    cat_mask = X.columns.isin(cat_variables_in_X)
+
+    complete_rows_mask = ~(X.isnull().sum(axis = 1) > 0)
+    
+    X_complete = X.loc[complete_rows_mask,:].copy()
+    X_complete = X_complete.apply(pd.to_numeric, errors="coerce")
+    X_complete = X_complete.astype('float64')
+
+    y_complete = y[complete_rows_mask].copy()
+
+    res = {}
+    res['X'] = X.copy()
+    res['y'] = y
+    res['cat_variables'] = cat_variables
+    res['cat_variables_in_X'] = cat_variables_in_X
+    res['cat_mask'] = cat_mask
+    res['id'] = id
+
+    res['X_complete'] = X_complete.copy()
+    res['y_complete'] = y_complete
+
+    res['num_classes'] = y_complete.value_counts().shape[0]
+
+
+    return res
+
 
 notebook = False
-       
 BASE_PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
 
 if notebook:
@@ -23,6 +85,7 @@ dataset_ids = pd.read_csv(dataset_ids_path, sep = " ")
 print(dataset_ids.head(3))
 
 datasets = dict.fromkeys(dataset_ids['Id'])
+
 
 for id in dataset_ids['Id']:
     print(f"Collecting dataset {id}")
